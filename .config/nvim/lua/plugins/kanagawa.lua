e1fca4cceffb60d9765695d5aa57ee69a9913fc5
thzyu <zyu-@outlook.com>
2025-05-10 23:01:33 +0800

    initial commit

---
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -0,0 +1,14 @@
+return {
+    "rebelot/kanagawa.nvim",
+    config = function()
+        require('kanagawa').setup({
+            compile = true,
+            theme = "dragon",              
+        })
+
+        vim.cmd("colorscheme kanagawa")
+    end,
+    build = function()
+        vim.cmd("KanagawaCompile")
+    end
+}
