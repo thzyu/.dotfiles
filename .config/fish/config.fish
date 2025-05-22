a206d238001a2611fc86522439486f0c7d4c5237
thzyu <zyu-@outlook.com>
2025-05-22 21:08:36 +0800

    update fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,17 @@ set fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/thzyu/miniconda3/bin/conda
+    eval /home/thzyu/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/home/thzyu/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/home/thzyu/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/thzyu/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
